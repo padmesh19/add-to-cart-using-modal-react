38b7d609935589c103325980cbd535d7e49d2f74
padmesh19 <padmesh191101@gmail.com>
2024-12-17 18:51:06 +0530

    initial commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ({ cartCount, onCartClick }) => {
         <h1 className="text-2xl text-blue-600 font-semibold"> Fake Store</h1>
       </div>
       <button
-        className="bg-[#ffffff40] px-4 py-2 rounded"
+        className="bg-[#ffffff40] px-4 py-2 font-medium rounded"
         onClick={onCartClick}
       >
         Cart ({cartCount})
